d17fa3a1f6318c548eb507dc0e9ffeaef6d93b63
goldenxing <705861485@qq.com>
2019-08-29 18:04:59 +0800

    d

---
+++ src/main/java/com/shaoxing/pojo/SysRole.java
@@ -0,0 +1,72 @@
+package com.shaoxing.pojo;
+
+public class SysRole {
+	private Integer id;
+	private String available;
+	private String description;
+	private String role;
+
+	public SysRole(Integer id, String available, String description, String role) {
+		this.id = id;
+		this.available = available;
+		this.description = description;
+		this.role = role;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the available
+	 */
+	public String getAvailable() {
+		return available;
+	}
+
+	/**
+	 * @param available the available to set
+	 */
+	public void setAvailable(String available) {
+		this.available = available;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
